26b6acb7c6aeb6601ffa31ba0b5e995fc5a4919a
Eugene Skepner <github@skepner.eu>
2022-02-18 15:44:35 +0100

    process

---
+++ process.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string_view>
+
+// ----------------------------------------------------------------------
+
+class Processor
+{
+  public:
+    Processor() = default;
+
+    void process(std::string_view chunk) {}
+};
+
+// ----------------------------------------------------------------------
--- read.cc
+++ read.cc
@@ -3,22 +3,45 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <cerrno>
-#include <cstring>
+#include <string>
+#include <array>
 #include <stdexcept>
 
 #include "read.hh"
+#include "process.hh"
 
 // ======================================================================
 
 void read_file_descriptor(int fd)
 {
+    using namespace std::string_literals;
+    if (fcntl(fd, F_SETFL, O_NONBLOCK) < 0)
+        throw std::runtime_error{"reading file description: "s + std::strerror(errno)};
+
+    constexpr size_t buffer_size = 1024;
+    std::array<char, buffer_size> buffer;
+
+    Processor processor;
     while (true) {
         fd_set fds;
         FD_ZERO(&fds);
         FD_SET(fd, &fds);
         const auto ret = select(fd + 1, &fds, nullptr, nullptr, nullptr);
         if (ret == -1)
-            throw std::runtime_error{std::strerror(errno)};
+            throw std::runtime_error{"selecting on file descriptor: "s + std::strerror(errno)};
+
+        const auto nread = read(fd, buffer.data(), buffer_size);
+        if (nread == -1) {
+            if (errno != EAGAIN)
+                throw std::runtime_error{"reading file description: "s + std::strerror(errno)};
+        }
+        else if (nread == 0) {  // EOF
+            break;
+        }
+        else {
+            processor.process(std::string_view(buffer.data(), static_cast<size_t>(nread)));
+        }
+
         // getline(std::cin, message);
         // if (std::cin.eof()) {
         //   std::cout << "eof." << std::endl;
@@ -73,8 +96,12 @@ class mmapped
 
 void read_mmap(const std::filesystem::path& filename)
 {
+    constexpr size_t chunk_size = 1024;
     mmapped mapped{filename};
-
+    Processor processor;
+    const std::string_view data = mapped;
+    for (size_t pos{0}; pos < data.size(); pos += chunk_size)
+        processor.process(data.substr(pos, chunk_size));
 }
 
 // ----------------------------------------------------------------------
