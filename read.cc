edd4d4b187a0b974d4a7b7645ddcf014038c10a1
Eugene Skepner <github@skepner.eu>
2022-02-18 15:23:52 +0100

    mmapped

--- read.cc
+++ read.cc
@@ -1,4 +1,7 @@
+#include <fcntl.h>
 #include <sys/select.h>
+#include <sys/types.h>
+#include <sys/mman.h>
 #include <cerrno>
 #include <cstring>
 #include <stdexcept>
@@ -27,12 +30,51 @@ void read_file_descriptor(int fd)
 
 // ----------------------------------------------------------------------
 
-void read_file(std::string_view filename)
+class mmapped
 {
-    if (filename == "-") {
-        read_stdin();
-        return;
+  public:
+    mmapped(const std::filesystem::path& filename)
+    {
+        if (std::filesystem::exists(filename)) {
+            mmapped_len_ = std::filesystem::file_size(filename);
+            fd_ = open(filename.c_str(), O_RDONLY);
+            if (fd_ >= 0) {
+                mmapped_ = reinterpret_cast<char*>(mmap(nullptr, mmapped_len_, PROT_READ, MAP_FILE | MAP_PRIVATE, fd_, 0));
+                if (mmapped_ == MAP_FAILED)
+                    throw std::runtime_error{"cannot read " + filename.native() + ": " + std::strerror(errno)};
+            }
+            else
+                throw std::runtime_error{"cannot open " + filename.native() + ": " + std::strerror(errno)};
+        }
+        else
+            throw std::runtime_error{filename.native() + ": no such file"};
     }
+    mmapped(const mmapped&) = delete;
+    mmapped operator=(const mmapped&) = delete;
+
+    ~mmapped()
+    {
+        if (fd_ > 2) {
+            if (mmapped_)
+                munmap(mmapped_, mmapped_len_);
+            close(fd_);
+        }
+    }
+
+    operator std::string_view() { return {mmapped_, mmapped_len_}; }
+
+  private:
+    int fd_{-1};
+    char* mmapped_{nullptr};
+    size_t mmapped_len_{0};
+};
+
+// ----------------------------------------------------------------------
+
+void read_mmap(const std::filesystem::path& filename)
+{
+    mmapped mapped{filename};
+
 }
 
 // ----------------------------------------------------------------------
--- read.hh
+++ read.hh
@@ -2,11 +2,21 @@
 
 #include <unistd.h>
 #include <string_view>
+#include <filesystem>
 
 // ----------------------------------------------------------------------
 
-void read_file(std::string_view filename);
 void read_file_descriptor(int fd);
+void read_mmap(const std::filesystem::path& filename);
 inline void read_stdin() { read_file_descriptor(STDIN_FILENO); }
+
+inline void read_file(const std::filesystem::path& filename)
+{
+    if (filename == "-")
+        read_stdin();
+    else
+        read_mmap(filename);
+}
+
 
 // ----------------------------------------------------------------------
